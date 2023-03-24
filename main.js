7d1d49de0bd9c80035a0009b6b2e73332cfa01cc
manojprabaker <manojprabaker999@gmail.com>
2023-03-24 12:20:41 +0530

    add

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var c1=parseInt(prompt("enter the month"));
+var a=new Date();
+a.setMonth(c1);
+var b=a.getMonth();
+
+if(b<3)
+{
+	document.write("HeyIts Fall");
+}
+else if (b<=c1 && b<7)
+{
+	document.write(" HeyIts Summer");
+}
+else if (7<=b && b<11)
+{
+	document.write("Its Winter");
+}
+else
+	{
+	document.write("Enter a valid month");
+}
+
